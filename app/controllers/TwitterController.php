52c40079a10f0146b3b909ea782ce4e391742537
Shashank Sanjay <ssanja1@pride.hofstra.edu>
2013-10-13 22:48:08 -0400

    configuring redirect

--- app/controllers/TwitterController.php
+++ app/controllers/TwitterController.php
@@ -14,6 +14,32 @@ class TwitterController extends \BaseController {
 
 	public function login()
 	{
+		if (Input::has('oauth_token'))
+		{
+		    //Return from redirect
+		    // Oauth token
+		    $token = Input::get('oauth_token');
+
+		    // Verifier token
+		    $verifier = Input::get('oauth_verifier');
+
+		    // Request access token
+		    $accessToken = Twitter::oAuthAccessToken($token, $verifier);
+
+		    dd($accessToken);
+		}
+		else {
+			//Redirect user to twitter
+			// Reqest tokens
+		    $tokens = Twitter::oAuthRequestToken();
+
+		    // Redirect to twitter
+		    Twitter::oAuthAuthorize(array_get($tokens, 'oauth_token'));
+		    exit;
+		}
+	}
+
+		/*
 		$twitter = new Twitter();
 		$c = Config::get('twitter.twitter');
 		$oauth = $twitter->oAuth($c['consumer_key'], $c['consumer_secret']); //$c['oauth_token'], $c['oauth_token_secret']);
@@ -21,7 +47,7 @@ class TwitterController extends \BaseController {
 
 		$requestToken = $oauth->getRequestToken();//($c['callback']);
 		$url = $oauth->getAuthorizeUrl($requestToken);
-		/*$oauth->getAccessToken();
+		$oauth->getAccessToken();
 
 		try {
 			$statuses = $twitter->call('statuses/home_timeline');
@@ -31,8 +57,6 @@ class TwitterController extends \BaseController {
 		} catch (Exception $e) {
 			return $e;
 		}*/
-		var_dump($url);
-	}
 
 }
 
