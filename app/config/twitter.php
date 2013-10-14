52b73ce6e5b5505f006291e8cdf675925d068dd7
Shashank Sanjay <ssanja1@pride.hofstra.edu>
2013-10-13 23:24:35 -0400

    fixed config

--- app/config/twitter.php
+++ app/config/twitter.php
@@ -8,7 +8,7 @@ return array (
 		'consumer_secret' => 'IWHeoUIYXPurlDSOW5lCOCwxLtUBiDPd5hSLU970',
 		'oauth_token' => '489525820-8R3wkIJWRhArr6zYYRRiHJ9RP4Hn6OOvGnzr5DOz',
 		'oauth_token_secret' => 'nqVJ1sO9b5wb2gW2glZGoppfqwOBWbIRJhhaFwilZQ',
-		'callback' => 'http://shashanksanjay.com/socialapp/login/twitter',
+		'callbackUrl' => 'http://shashanksanjay.com/socialapp/login/twitter',
 		)
 	);
 
