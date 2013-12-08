6fd1e10259057146268b6011c498808efb418024
Server <server@shashanksanjay.com>
2013-12-07 20:50:04 -0700

    argh json

--- app/routes.php
+++ app/routes.php
@@ -77,20 +77,20 @@ Route::get('/fballpages', function ()
 	$pageIDs = array($data[0]['id']);
 	foreach ($pageIDs as $key => $page)
 	{
-		$fp = fopen('', 'w');
-		
+				
 		// set the url, number of POST vars, POST data
 		// curl_setopt($ch, CURLOPT_URL, $url);
 		// curl_setopt($ch, CURLOPT_FILE, $fp);
 		// curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_POST, 2);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, '/?access_token=' . $data[0]['access_token'] . '&batch=[{"method":"GET", "relative_url":"me/' . $page . '?fields=id,access_token,likes,admins,albums,conversations,events,feed,insights,links,locations,posts,questions,statuses,tagged,videos"}]');
+		curl_setopt($ch, CURLOPT_POSTFIELDS, 'access_token=' . $data[0]['access_token'] . '&batch=[{"method":"GET", "relative_url":"/' . $data[0]['id'] . '?fields=id,access_token,likes,admins,albums,conversations,events,feed,insights,links,locations,posts,questions,statuses,tagged,videos"}]');
 		
-		$result = curl_exec($ch);
-		echo '<pre>' . $result . '</pre>';
+		$result = json_decode(curl_exec($ch));
+		echo '<pre>';
+		print_r($result);
+		echo '</pre>';
 	}
 	curl_close($ch);
-	fclose($fp);
 	
 });
 
