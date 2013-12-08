d6a444ec8177ef0af799b806694db35e56aaf66d
Shashank Sanjay <ssanja1@pride.hofstra.edu>
2013-12-07 22:36:18 -0500

    we'll see what happens

--- app/routes.php
+++ app/routes.php
@@ -71,7 +71,26 @@ Route::get('/fballpages', function ()
 	echo '</pre>';	
 	*/
 	
-	FacebookRetriever::batchCall(array($data[0]['id']), $data[0]['access_token']);
+	//FacebookRetriever::batchCall(array($data[0]['id']), $data[0]['access_token']);
+
+	$ch = curl_init('https://graph.facebook.com/');
+	$pageIDs = array($data[0]['id']);
+	foreach ($pageIDs as $key => $page)
+	{
+		$fp = fopen('', 'w');
+		
+		// set the url, number of POST vars, POST data
+		// curl_setopt($ch, CURLOPT_URL, $url);
+		// curl_setopt($ch, CURLOPT_FILE, $fp);
+		// curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_POST, 2);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, '/?access_token=' . $data[0]['access_token'] . '&batch=[{"method":"GET", "relative_url":"me/' . $page . '?fields=id,access_token,likes,admins,albums,conversations,events,feed,insights,links,locations,posts,questions,statuses,tagged,videos"}]');
+		
+		$result = curl_exec($ch);
+		echo '<pre>' . $result . '</pre>';
+	}
+	curl_close($ch);
+	fclose($fp);
 	
 });
 
