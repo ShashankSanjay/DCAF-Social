41e709edad68f3dfe0555a7426dac6b0cfe19359
shashanksanjay <ssanja1@pride.hofstra.edu>
2014-01-16 13:12:50 -0500

    sucked

--- app/routes.php
+++ app/routes.php
@@ -108,4 +108,4 @@ Route::get('{postSlug}', 'BlogController@getView');
 Route::post('{postSlug}', 'BlogController@postView');
 
 # Index Page - Last route, no matches
-Route::get('/', array('before' => 'detectLang','uses' => 'BlogController@getIndex'));
+Route::get('/', array('before' => 'auth','uses' => 'BlogController@getIndex'));
