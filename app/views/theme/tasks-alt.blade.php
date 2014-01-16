dec43b786df17adf1c1280edbd61064fb86993a9
Alexander Rosenberg <alex_rosenberg35@yahoo.com>
2014-01-16 15:16:24 -0500

    fixed blad files, again

--- app/views/theme/tasks-alt.blade.php
+++ app/views/theme/tasks-alt.blade.php
@@ -18,38 +18,38 @@
 		<link href='http://fonts.googleapis.com/css?family=Share+Tech' rel='stylesheet' type='text/css'>
 		
 		<!-- Bootstrap core CSS -->
-		{{ HTML::style('bootstrap/css/bootstrap.min-50686.css'); }}
+		{{ HTML::style('bootstrap/css/bootstrap.min-50686.css') }}
 
 		<!-- Font Awesome CSS -->
-		{{ HTML::style('fonts/font-awesome/css/font-awesome.min-45579.css'); }}
+		{{ HTML::style('fonts/font-awesome/css/font-awesome.min-45579.css') }}
 
 		<!-- Icomoon CSS -->
-		{{ HTML::style('fonts/icomoon/style.css'); }}
+		{{ HTML::style('fonts/icomoon/style.css') }}
 
 		<!-- Animate CSS -->
-		{{ HTML::style('css/libs/animate.min.css'); }}
+		{{ HTML::style('css/libs/animate.min.css') }}
 
 		<!-- Bootstrap Switch -->
-		{{ HTML::style('css/libs/bootstrap-switch.css'); }}
+		{{ HTML::style('css/libs/bootstrap-switch.css') }}
 
 		<!-- Bootstrap Select -->
-		{{ HTML::style('css/libs/bootstrap-select.min.css'); }}
+		{{ HTML::style('css/libs/bootstrap-select.min.css') }}
 
 		<!-- Bootstrap WYSIHTML5 -->
-		{{ HTML::style('css/libs/bootstrap-wysihtml5.css'); }}
+		{{ HTML::style('css/libs/bootstrap-wysihtml5.css') }}
 
 		<!-- jQuery Fullcalendar -->
-		{{ HTML::style('css/libs/fullcalendar.css'); }}
+		{{ HTML::style('css/libs/fullcalendar.css') }}
 
 		<!-- jVectorMap -->
-		{{ HTML::style('css/libs/jquery-jvectormap-1.2.2.css'); }}
+		{{ HTML::style('css/libs/jquery-jvectormap-1.2.2.css') }}
 
 		<!-- Prism -->
-		{{ HTML::style('css/libs/prism.css'); }}
+		{{ HTML::style('css/libs/prism.css') }}
 
 		<!-- Custom styles for this template -->
-		<link href="{{{ asset('assets/css/styler/style.css'); }}}" rel="stylesheet" type="text/css">
-		<link href="{{{ asset('assets/css/demo.css'); }}}" rel="stylesheet" type="text/css">
+		<link href="{{{ asset('assets/css/styler/style.css') }}}" rel="stylesheet" type="text/css">
+		<link href="{{{ asset('assets/css/demo.css') }}}" rel="stylesheet" type="text/css">
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -914,65 +914,65 @@
 		</footer>
 
 		<!-- jQuery -->
-		{{ HTML::script('js/libs/jquery-1.10.2.min.js'); }}
+		{{ HTML::script('js/libs/jquery-1.10.2.min.js') }}
 
 		<!-- jQuery UI -->
-		{{ HTML::script('js/libs/jquery-ui.min.js'); }}
+		{{ HTML::script('js/libs/jquery-ui.min.js') }}
 
 		<!-- Bootstrap core JavaScript -->
-		{{ HTML::script('bootstrap/js/bootstrap.min.js?v=3.0.2'); }}
+		{{ HTML::script('bootstrap/js/bootstrap.min.js?v=3.0.2') }}
 
 		<!-- jQuery Transit -->
-		{{ HTML::script('js/libs/jquery.transit.min.js?v=0.9.9'); }}
+		{{ HTML::script('js/libs/jquery.transit.min.js?v=0.9.9') }}
 
 		<!-- Bootstrap Switch -->
-		{{ HTML::script('js/libs/bootstrap-switch.js'); }}
+		{{ HTML::script('js/libs/bootstrap-switch.js') }}
 
 		<!-- Bootstrap Select -->
-		{{ HTML::script('js/libs/bootstrap-select.min.js'); }}
+		{{ HTML::script('js/libs/bootstrap-select.min.js') }}
 
 		<!-- Bootstrap File -->
-		{{ HTML::script('js/libs/bootstrap-filestyle.js'); }}
+		{{ HTML::script('js/libs/bootstrap-filestyle.js') }}
 
-		{{ HTML::script('js/libs/wysihtml5-0.3.0.min.js'); }}
+		{{ HTML::script('js/libs/wysihtml5-0.3.0.min.js') }}
 
 		<!-- Bootstrap WYSIHTML5 -->
-		{{ HTML::script('js/libs/bootstrap-wysihtml5.js'); }}
+		{{ HTML::script('js/libs/bootstrap-wysihtml5.js') }}
 
 		<!-- jQuery FullCalendar -->
-		{{ HTML::script('js/libs/fullcalendar.min.js'); }}
-		{{ HTML::script('js/libs/gcal.js'); }}
+		{{ HTML::script('js/libs/fullcalendar.min.js') }}
+		{{ HTML::script('js/libs/gcal.js') }}
 
 		<!-- Prism -->
-		{{ HTML::script('js/libs/prism.js'); }}
+		{{ HTML::script('js/libs/prism.js') }}
 
 		<!-- jVectorMap -->
-		{{ HTML::script('js/libs/jquery-jvectormap-1.2.2.min.js'); }}
-		{{ HTML::script('js/libs/jquery-jvectormap-world-mill-en.js'); }}
+		{{ HTML::script('js/libs/jquery-jvectormap-1.2.2.min.js') }}
+		{{ HTML::script('js/libs/jquery-jvectormap-world-mill-en.js') }}
 
 		<!-- Flot -->
-		{{ HTML::script('js/libs/jquery.flot.min.js'); }}
-		{{ HTML::script('js/libs/jquery.flot.time.min.js'); }}
-		{{ HTML::script('js/libs/jquery.flot.pie.min.js'); }}
-		{{ HTML::script('js/libs/jquery.flot.resize.min.js'); }}
-		{{ HTML::script('js/libs/jquery.flot.stack.min.js'); }}
-		{{ HTML::script('js/libs/jquery.flot.tooltip.min.js'); }}
+		{{ HTML::script('js/libs/jquery.flot.min.js') }}
+		{{ HTML::script('js/libs/jquery.flot.time.min.js') }}
+		{{ HTML::script('js/libs/jquery.flot.pie.min.js') }}
+		{{ HTML::script('js/libs/jquery.flot.resize.min.js') }}
+		{{ HTML::script('js/libs/jquery.flot.stack.min.js') }}
+		{{ HTML::script('js/libs/jquery.flot.tooltip.min.js') }}
 
 		<!-- Sparkline -->
-		{{ HTML::script('js/libs/jquery.sparkline.min.js'); }}
+		{{ HTML::script('js/libs/jquery.sparkline.min.js') }}
 
 		<!-- Prism -->
-		{{ HTML::script('js/libs/jquery.sparkline.min.js'); }}
+		{{ HTML::script('js/libs/jquery.sparkline.min.js') }}
 
 		<!-- jQuery EqualHeights -->
-		{{ HTML::script('js/libs/jquery.equalheights.min.js'); }}
+		{{ HTML::script('js/libs/jquery.equalheights.min.js') }}
 
 		<!-- jQuery Nicescroll -->
-		{{ HTML::script('js/libs/jquery.nicescroll.min.js'); }}
+		{{ HTML::script('js/libs/jquery.nicescroll.min.js') }}
 
 		<!-- Theme script -->
-		{{ HTML::script('js/styler/script.js'); }}
-		{{ HTML::script('js/styler/sample_graphs.js'); }}
+		{{ HTML::script('js/styler/script.js') }}
+		{{ HTML::script('js/styler/sample_graphs.js') }}
 	</body>
 </html>
 <!-- Localized -->
