c2b40fab56596a6fbfb1fc059937fb70972797e1
Alexander Rosenberg <alex_rosenberg35@yahoo.com>
2014-01-16 13:21:54 -0500

    added hyperia theme

---
+++ app/models/DcafUser.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class User
+{
+	private $id;
+	private $username;
+	private $gender;
+	private $email;
+	private $firstName;
+	private $lastName;
+	private $likes = array();
+	
+	
+}
+
+?>
