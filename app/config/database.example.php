f21acf1bb8996ae5947fb73f50265791af54105d
Shashank Sanjay <ssanja1@pride.hofstra.edu>
2014-01-20 16:27:09 -0500

    Update and rename database.php to database.example.php

--- app/config/database.example.php
+++ app/config/database.example.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'shashan2_DCAF-Social',
-			'username'  => 'shashan2_DCAF',
-			'password'  => 'DatabaseBean',
+			'database'  => 'laravelbootstrapstarter',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
