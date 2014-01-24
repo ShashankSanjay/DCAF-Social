0942201b62d63740bbb421d4e730410db5c1ceb2
shashanksanjay <ssanja1@pride.hofstra.edu>
2014-01-24 15:19:51 -0500

    ayo

--- app/views/site/dashboard/home.blade.php
+++ app/views/site/dashboard/home.blade.php
@@ -4,6 +4,20 @@
 @section('content')
   <div id="content">
 		<div class="container">
+			<style type="text/css">
+				#sidebar {
+					background-color: 
+					/*rgb(22, 145, 190) !important;
+					rgb(22, 107, 162) !important;
+					rgb(27, 54, 71) !important;*/
+					
+				}
+				h1 {
+					color: #222 !important;
+					text-shadow: 0px 2px 3px #555 !important;
+				}
+			</style>
+			
 			<h1>Welcome to DCAF</h1>
 			
 
