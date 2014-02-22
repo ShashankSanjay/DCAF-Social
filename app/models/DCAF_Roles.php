8184828961c8702fbcc4be9cbfb6e7e993b8696f
Server <server@shashanksanjay.com>
2014-02-22 12:38:50 -0700

    updated DCAF_Roles

--- app/models/DCAF_Roles.php
+++ app/models/DCAF_Roles.php
@@ -1,20 +1,51 @@
 <?php
 
-class DCAF_Roles extends Eloquent {
-    protected $guarded = array();
-    
-    protected $table = 'DCAF_Roles';
-    
-    public static $rules = array();
+use Zizaco\Entrust\EntrustRole;
+use Robbo\Presenter\PresentableInterface;
 
-    protected $roleId;
-    protected $roleName;
-
-    public function DCAF_Users() 
-    {
-    	//
-    	return $this->belongsToMany('DCAF_User', 'DCAF_User_Roles', 'role_id', 'uid');
-    }
+class DCAF_Role extends EntrustRole implements PresentableInterface
+{
+	protected $guarded = array();
+	
+	protected $table = 'DCAF_Roles';
+	
+	public static $rules = array();
+	
+	protected $roleId;
+	protected $roleName;
+	
+	public function DCAF_Users()
+	{
+		return $this->belongsToMany('DCAF_User', 'DCAF_User_Roles', 'role_id', 'uid');
+	}
+	
+	/**
+	 * Same presenter as the user model.
+	 * 
+	 * @return Robbo\Presenter\Presenter|UserPresenter
+	 */
+	public function getPresenter()
+	{
+		return new UserPresenter($this);
+	}
+	
+	/**
+	 * Provide an array of strings that map to valid roles.
+	 * 
+	 * @param array $roles
+	 * @return stdClass
+	 */
+	public function validateRoles( array $roles )
+	{
+		$user = Confide::user();
+		$roleValidation = new stdClass();
+		foreach( $roles as $role )
+		{
+			// Make sure theres a valid user, then check role.
+			$roleValidation->$role = ( empty($user) ? false : $user->hasRole($role) );
+		}
+		return $roleValidation;
+	}
 }
 
 ?>
