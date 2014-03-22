51d638c9fd2c65ffeda99692959c32d70d18bd9c
shashanksanjay <ssanja1@pride.hofstra.edu>
2014-03-22 03:01:59 -0400

    stuff

--- app/database/seeds/PermissionsTableSeeder.php
+++ app/database/seeds/PermissionsTableSeeder.php
@@ -4,14 +4,12 @@ class PermissionsTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::table('permissions')->delete();
+        //DB::table('permissions')->delete();
 
-        $manageBilling = new Permission();
-        $manageBilling->id = 1;
-        $manageBilling->name = 'manage_billing';
-        $manageBilling->display_name = 'manage billing';
-        $manageBilling->save();
-        
+        $manageBilling = new Permission::firstOrCreate(array(;
+            'name' => 'manage_billing',
+            'display_name' => 'manage billing',
+        );
         //$r->perms()->attach($manageBilling); //works
         
         //$r->perms()->sync(array($manageBilling->id)); //works
